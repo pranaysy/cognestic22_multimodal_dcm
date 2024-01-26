c95599eeab965c2fb4b688c40438781496c97530
Pranay Yadav <py246@cam.ac.uk>
2024-01-26 11:40:36 +0000

    Updated batch for PEB with covariate

--- code/meg/saved_from_batch_interface/batch_peb_covariate_job.m
+++ code/meg/saved_from_batch_interface/batch_peb_covariate_job.m
@@ -1,29 +1,29 @@
 %-----------------------------------------------------------------------
-% Job saved on 10-Nov-2022 18:57:13 by cfg_util (rev $Rev: 7345 $)
+% Job saved on 25-Jan-2024 13:08:38 by cfg_util (rev $Rev: 7345 $)
 % spm SPM - SPM12 (7771)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.dcm.peb.specify.name = 'Age';
-matlabbatch{1}.spm.dcm.peb.specify.model_space_mat = {'/imaging/henson/Wakeman/cognestic22_multimodal_dcm/fits/batch_gui/meg/GCM_Full.mat'};
+matlabbatch{1}.spm.dcm.peb.specify.model_space_mat = {'/imaging/henson/Wakeman/pranay_does_things/CBU_Neuroimaging_2024_Test_Reset/fits/batch_gui/meg/GCM_Full.mat'};
 matlabbatch{1}.spm.dcm.peb.specify.dcm.index = 1;
 matlabbatch{1}.spm.dcm.peb.specify.cov.regressor.name = 'Age';
 %%
-matlabbatch{1}.spm.dcm.peb.specify.cov.regressor.value = [4.625
-                                                          -1.375
-                                                          3.625
-                                                          -0.375
-                                                          -3.375
-                                                          -0.375
-                                                          4.625
-                                                          -0.375
-                                                          2.625
-                                                          -3.375
-                                                          -2.375
-                                                          -2.375
-                                                          -1.375
-                                                          -2.375
-                                                          3.625
-                                                          -1.375];
+matlabbatch{1}.spm.dcm.peb.specify.cov.regressor.value = [4.6
+                                                          -1.4
+                                                          3.6
+                                                          -0.4
+                                                          -3.4
+                                                          -0.4
+                                                          4.6
+                                                          -0.4
+                                                          2.6
+                                                          -3.4
+                                                          -2.4
+                                                          -2.4
+                                                          -1.4
+                                                          -2.4
+                                                          3.6
+                                                          -1.4];
 %%
 matlabbatch{1}.spm.dcm.peb.specify.fields.custom = {'B'};
 matlabbatch{1}.spm.dcm.peb.specify.priors_between.components = 'All';
